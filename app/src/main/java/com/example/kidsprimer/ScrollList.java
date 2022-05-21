07abae5317364b201322ec74c1313ee3eb553dde
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-05-21 13:34:21 +0500

    PRIMER LIST UI COMPLETED

--- app/src/main/java/com/example/kidsprimer/ScrollList.java
+++ app/src/main/java/com/example/kidsprimer/ScrollList.java
@@ -1,11 +1,16 @@
 package com.example.kidsprimer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
 
 public class ScrollList extends AppCompatActivity {
 
+    TextView [] t = new TextView[26];
     int[] ids = {R.id.textView4,R.id.textView5,R.id.textView6,R.id.textView7,R.id.textView8,
             R.id.textView9,R.id.textView10,R.id.textView11,
                 R.id.textView12,R.id.textView13,R.id.textView14,R.id.textView15,R.id.textView16,
@@ -16,6 +21,17 @@ public class ScrollList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll_list);
-
+        TextView head = findViewById(R.id.textView3);
+        head.setTextColor(Color.RED);
+        head.setGravity(Gravity.CENTER);
+        head.setBackgroundColor(getResources().getColor(R.color.cyan));
+        for (int i = 0; i < ids.length; i++) {
+            t[i] = findViewById(ids[i]);
+            t[i].setTextColor(Color.RED);
+            t[i].setTextSize(40);
+            t[i].setHeight(200);
+            t[i].setBackground(getDrawable(R.drawable.textview_border));
+            t[i].setGravity(Gravity.CENTER);
+        }
     }
 }
