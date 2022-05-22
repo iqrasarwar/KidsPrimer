c40c1547c12c77fc8deababc0f96a4c4d0c266ce
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-05-22 13:48:52 +0500

    link to repo switching learn module

--- app/src/main/java/com/example/kidsprimer/SplachActivity.java
+++ app/src/main/java/com/example/kidsprimer/SplachActivity.java
@@ -2,13 +2,35 @@ package com.example.kidsprimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplachActivity extends AppCompatActivity {
-
+    Button lern , test, repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = findViewById(R.id.button);
+        lern = findViewById(R.id.button2);
+        repo = findViewById(R.id.button3);
+        lern.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplachActivity.this,ScrollList.class);
+                startActivity(intent);
+            }
+        });
+        repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/iqrasarwar/KidsPrimer");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
     }
 }
