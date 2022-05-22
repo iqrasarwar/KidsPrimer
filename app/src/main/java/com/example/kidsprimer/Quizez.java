c691c65faf6b7d3d1ef88ecbc7306a5c23cc9a11
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-05-22 21:10:35 +0500

    quiz 4 revert

--- app/src/main/java/com/example/kidsprimer/Quizez.java
+++ app/src/main/java/com/example/kidsprimer/Quizez.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class Quizez extends AppCompatActivity {
-    TextView q1,q2,q3,q4;
+    TextView q1,q2,q3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +16,6 @@ public class Quizez extends AppCompatActivity {
         q1 = findViewById(R.id.q1);
         q2 = findViewById(R.id.q2);
         q3 = findViewById(R.id.q3);
-        q4 = findViewById(R.id.q4);
         q1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,13 +34,6 @@ public class Quizez extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Quizez.this,quiz3.class);
-                startActivity(intent);
-            }
-        });
-        q4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Quizez.this,quiz4.class);
                 startActivity(intent);
             }
         });
