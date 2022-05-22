d9b726b1381f28c1e20a040b9c68b979ef138823
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-05-22 16:58:18 +0500

    quizez switching listners

--- app/src/main/java/com/example/kidsprimer/Quizez.java
+++ app/src/main/java/com/example/kidsprimer/Quizez.java
@@ -2,13 +2,48 @@ package com.example.kidsprimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Quizez extends AppCompatActivity {
-
+    TextView q1,q2,q3,q4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizez);
+        q1 = findViewById(R.id.q1);
+        q2 = findViewById(R.id.q2);
+        q3 = findViewById(R.id.q3);
+        q4 = findViewById(R.id.q4);
+        q1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Quizez.this,ScrollList.class);
+                startActivity(intent);
+            }
+        });
+        q2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Quizez.this,ScrollList.class);
+                startActivity(intent);
+            }
+        });
+        q3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Quizez.this,ScrollList.class);
+                startActivity(intent);
+            }
+        });
+        q4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Quizez.this,ScrollList.class);
+                startActivity(intent);
+            }
+        });
     }
 }
