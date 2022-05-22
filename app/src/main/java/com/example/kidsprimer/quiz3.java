571294c7835a9340ec230e22c32b92cdb2805293
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-05-22 17:03:27 +0500

    4 quizez activities addded

---
+++ app/src/main/java/com/example/kidsprimer/quiz3.java
@@ -0,0 +1,14 @@
+package com.example.kidsprimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quiz3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz3);
+    }
+}
---
+++ app/src/main/java/com/example/kidsprimer/quiz4.java
@@ -0,0 +1,14 @@
+package com.example.kidsprimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quiz4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz4);
+    }
+}
