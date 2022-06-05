ef2c1f0623ae13095b119c6db672e78d4ed84194
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-05 08:55:36 +0500

    list configuration added

---
+++ app/src/main/java/com/example/kidsprimer/CatagConfig.java
@@ -0,0 +1,37 @@
+package com.example.kidsprimer;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CatagConfig extends ArrayAdapter<CatagList> {
+    public CatagConfig(@NonNull Context context, int resource,
+                       @NonNull List<CatagList> objects) {
+        super(context, resource, objects);
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        CatagList ctg =  getItem(position);
+        convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_view_for_catagories,parent,
+                false);
+        TextView txt = convertView.findViewById(R.id.textView44);
+        ImageView arrow = convertView.findViewById(R.id.imageView5);
+        ImageView img = convertView.findViewById(R.id.imageView3);
+        txt.setText(ctg.getTxt());
+        img.setImageResource(ctg.getImg());
+        arrow.setImageResource(ctg.getArrow());
+        return convertView;
+    }
+}
