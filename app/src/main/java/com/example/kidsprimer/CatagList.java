0d78bf4d596d39c94bf36b708b5bb335d2127ffc
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-05 08:54:44 +0500

    list item class added

---
+++ app/src/main/java/com/example/kidsprimer/CatagList.java
@@ -0,0 +1,40 @@
+package com.example.kidsprimer;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CatagList {
+    private int img;
+    private String txt;
+    private int arrow;
+
+    public CatagList(int img, String txt, int arrow) {
+        this.img = img;
+        this.txt = txt;
+        this.arrow = arrow;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getTxt() {
+        return txt;
+    }
+
+    public void setTxt(String txt) {
+        this.txt = txt;
+    }
+
+    public int getArrow() {
+        return arrow;
+    }
+
+    public void setArrow(int arrow) {
+        this.arrow = arrow;
+    }
+}
