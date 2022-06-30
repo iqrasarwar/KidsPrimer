4179801571522222552616af6916e461d47f456a
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-30 19:29:26 +0500

    splash otrait added

--- app/src/main/java/com/example/kidsprimer/SplashActivity.java
+++ app/src/main/java/com/example/kidsprimer/SplashActivity.java
@@ -3,12 +3,31 @@ package com.example.kidsprimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 
+import android.view.WindowManager;
 public class SplashActivity extends AppCompatActivity {
-
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,
+                        MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
+
 }
+
+
