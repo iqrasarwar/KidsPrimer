2d028e231ef780da7b723d44667c7ca78484f525
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-07-02 02:06:27 +0500

    main & slash 2 modes com

--- app/src/main/java/com/example/kidsprimer/MainActivity.java
+++ app/src/main/java/com/example/kidsprimer/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.kidsprimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer bg;
     Button lern , test, repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         test = findViewById(R.id.button);
         lern = findViewById(R.id.button2);
         repo = findViewById(R.id.button3);
+        bg = MediaPlayer.create(MainActivity.this,R.raw.kids);
+        bg.start();
         lern.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,5 +43,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bg.release();
     }
 }
