a190b1c8e0a3706c6691f297f1807229ff535e92
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-07-02 00:23:22 +0500

    bgm addedd

--- app/src/main/java/com/example/kidsprimer/SplashActivity.java
+++ app/src/main/java/com/example/kidsprimer/SplashActivity.java
@@ -2,13 +2,15 @@ package com.example.kidsprimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Handler;
 
 import android.view.WindowManager;
 public class SplashActivity extends AppCompatActivity {
-    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    MediaPlayer bg;
+    private static int SPLASH_SCREEN_TIME_OUT=5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,8 @@ public class SplashActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
+        bg = MediaPlayer.create(SplashActivity.this,R.raw.kids);
+        bg.start();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -27,7 +31,12 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, SPLASH_SCREEN_TIME_OUT);
     }
-
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        bg.release();
+    }
 }
 
 
