4e18099d6ea499bfd9db4427a7d99c5aaaf0cc2e
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-07-03 23:25:38 +0500

    slider activity added

--- app/src/main/java/com/example/kidsprimer/Catogries.java
+++ app/src/main/java/com/example/kidsprimer/Catogries.java
@@ -9,14 +9,23 @@ import java.util.ArrayList;
 
 public class Catogries extends AppCompatActivity {
     ListView listview;
+    String [] listNames = {"Alphabets A to Z","Rhymes","Numbers 1 to 100","Days of week","Months " +
+            "of year","Color Names","Fruit Names","Flower Names","Animal Names","Bird Names",
+            "Vegetable Names","Profession Names","Shape Names","Country Names","Know the Pakistan"};
+    int [] icons = {R.drawable.alphabets,R.drawable.rhymes,R.drawable.counting,
+            R.drawable.weekdays,R.drawable.months,R.drawable.colors,R.drawable.fruit,
+            R.drawable.flowrs,R.drawable.animals,R.drawable.birds,R.drawable.vegtables,
+            R.drawable.professions,R.drawable.shapes,R.drawable.country, R.drawable.pakistan};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catogries);
         listview = findViewById(R.id.CatagoriesList);
         ArrayList<CatagList> arraylist = new ArrayList<CatagList>();
-        arraylist.add(new CatagList(R.drawable.rhymes,"Rhymes",
-                R.drawable.ic_baseline_double_arrow_24));
+        for (int i = 0; i < 15; i++) {
+            arraylist.add(new CatagList(icons[i],listNames[i],
+                    R.drawable.ic_baseline_double_arrow_24));
+        }
         CatagConfig list = new CatagConfig(this,0,arraylist);
         listview.setAdapter(list);
     }
---
+++ app/src/main/java/com/example/kidsprimer/Slider.java
@@ -0,0 +1,14 @@
+package com.example.kidsprimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Slider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider);
+    }
+}
